8ee9e00a7f6c7cf2a5a478ce673deda7bfac3bd5
DrAP <agus10pg@gmail.com>
2021-10-03 13:58:18 +0200

    Create project

---
+++ app/src/main/java/com/example/CovidApp.kt
@@ -0,0 +1,27 @@
+package com.example
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+
+class CovidApp : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    companion object {
+        lateinit var instance: CovidApp
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}
